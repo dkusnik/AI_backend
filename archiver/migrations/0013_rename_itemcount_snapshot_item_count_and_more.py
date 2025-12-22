7f9d622262b0b2c9600586938cafd7cb4980e068
Damian Kusnik <dkusnik@polsl.pl>
2025-12-22 19:27:48 +0100

    Fix migrations

--- archiver/migrations/0013_rename_itemcount_snapshot_item_count_and_more.py
+++ archiver/migrations/0013_rename_itemcount_snapshot_item_count_and_more.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('archiver', '0012_alter_snapshot_website'),
+        ('archiver', '0010_snapshot_uid'),
     ]
 
     operations = [
